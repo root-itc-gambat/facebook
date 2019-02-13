7fa6f143d111a8a3ebd25e7b746d355581bab304
root-itc-gambat <rootsuperuser02@gmail.com>
2019-02-13 15:07:59 +0500

    Updated index.php

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
+<form action="" method="get">
+    Enter name : <input type="text" name="friend_name"/>
+    <input type="submit" value="Add Friend"/>
+</form>
+
 <?php
-
-    echo "Welcome Back Again n Again";
-
+    $f_name = $_GET['friend_name'];
+    
+    echo "$f_name";
 ?>
